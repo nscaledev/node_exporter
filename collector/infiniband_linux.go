f7d4a7eee24f29902fe067be6056cb2dc6b49073
Antony Cleave <antony.cleave@nscale.com>
2024-06-18 17:44:59 +0100

    fix typo

--- collector/infiniband_linux.go
+++ collector/infiniband_linux.go
@@ -85,6 +85,7 @@ func NewInfiniBandCollector(logger log.Logger) (Collector, error) {
 		"port_receive_switch_relay_errors_total":     "Number of packets that could not be forwarded by the switch.",
 		"symbol_error_total":                         "Number of minor link errors detected on one or more physical lanes.",
 		"vl15_dropped_total":                         "Number of incoming VL15 packets dropped due to resource limitations.",
+		"port_receive_roce_bytes":                    "Number of RoCE data octets received",
 	}
 
 	i.metricDescs = make(map[string]*prometheus.Desc)
@@ -170,7 +171,7 @@ func (c *infinibandCollector) Update(ch chan<- prometheus.Metric) error {
 			c.pushCounter(ch, "symbol_error_total", port.Counters.SymbolError, port.Name, portStr)
 			c.pushCounter(ch, "vl15_dropped_total", port.Counters.VL15Dropped, port.Name, portStr)
 
-			c.pushCounter(ch, "port_recieve_roce_bytes", port.HwCounters.RxRoceOnlyBytes, port.Name, portStr)
+			c.pushCounter(ch, "port_receive_roce_bytes", port.HwCounters.RxRoceOnlyBytes, port.Name, portStr)
 		}
 	}
 
